254b33d8afd21c2e4a13b29ba0600b0f082a3924
RohitPravisht <rohit.chahal@pravisht.com>
2024-09-01 01:29:40 +0530

    initial commit

---
+++ src/utils/Images.ts
@@ -0,0 +1,8 @@
+const url = '../../assets/images/';
+
+const images = {
+  //   activity: require(url + 'activity.png'),
+  //   arrow_up: require(url + 'arrow-up.png'),
+};
+
+export default images;
---
+++ src/utils/LottieFiles.ts
@@ -0,0 +1,15 @@
+// const url = '../../assets/lottie/';
+
+// const jsonFiles = {
+//   no_data: require(url + 'no_data.json'),
+//   coming_soon: require(url + 'coming_soon.json'),
+//   loader: require(url + 'loader.json'),
+//   splash: require(url + 'splash.json'),
+//   no_data_found: require(url + 'no_data_found.json'),
+//   no_notification: require(url + 'no_notification.json'),
+//   congratulations: require(url + 'congratulations.json'),
+//   dashboard: require(url + 'dashboard.json'),
+//   empty: require(url + 'empty.json'),
+// };
+
+// export default jsonFiles;
